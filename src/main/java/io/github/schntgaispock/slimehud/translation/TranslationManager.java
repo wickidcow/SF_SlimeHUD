826ac2fdf01d856be0df3216372aac3221fdd073
wickidcow <jaywago@gmail.com>
2026-09-15 21:48:41 -0400

    Add region-safe Slimefun item name overload

--- src/main/java/io/github/schntgaispock/slimehud/translation/TranslationManager.java
+++ src/main/java/io/github/schntgaispock/slimehud/translation/TranslationManager.java
@@ -6,6 +6,10 @@ import org.bukkit.entity.Player;
 public final class TranslationManager {
 
     public String getItemName(Player player, SlimefunItem sfItem) {
+        return getItemName(sfItem);
+    }
+
+    public String getItemName(SlimefunItem sfItem) {
         return sfItem.getItemName();
     }
 }
